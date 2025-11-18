481e4e3e2cc2ae629c19aaf8b6971b1b1f08b5a1
Pruthviraj250304 <pruthvirajbayas25@gmail.com>
2025-11-18 13:41:04 +0530

    Express in node

---
+++ app.js
@@ -0,0 +1,23 @@
+const express = require('express')
+require('dotenv').config()
+
+
+const app = express()
+
+const port =   process.env.PORT  || 7000
+
+app.get('/',(req,res)=>{
+    res.send('Hello from express..')
+})
+
+app.get('/contactus', (req,res)=>{
+    res.send("<h1>This is Contact us page</h1>")
+})
+
+app.post('/aboutus',(req,res,next)=>{
+    res.send('This is about page ')
+})
+
+app.listen(port,()=>{
+    console.log(`server running on http://localhost:${port}`)
+})
